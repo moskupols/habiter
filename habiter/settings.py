70a70afa22ce48c3684dcd90135038b6c876da64
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-07-02 19:50:30 +0300

    Add test user credentials

--- habiter/settings.py
+++ habiter/settings.py
@@ -1,7 +1,8 @@
 try:
     from .secret import user_id, api_key
 except ImportError:
-    raise ImportError('you should create secret.py and initialize user_id and api_key there ._.')
+    user_id = 'c31fdbc0-15c5-44a8-8ef9-c8c8842b0266'
+    api_key = '085e09c3-9a28-4828-a9e5-8dfb953f2dea'
 
 palette = (
     ('list_title', 'bold', ''),
