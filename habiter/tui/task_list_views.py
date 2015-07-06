cdde109506316aa725ebaa23b22de5478e02426e
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-07-06 18:20:27 +0300

    Filter tasks, not widgets

--- habiter/tui/task_list_views.py
+++ habiter/tui/task_list_views.py
@@ -1,71 +1,81 @@
 import itertools
 import urwid
 from habiter.models.list_model import MappingListProxyModel, FilterListProxyModel
-from habiter.settings import ACCEL_TOGGLE_LIST_MODE
+from habiter.settings import ACCEL_NEXT_LIST_FILTER
 from habiter.tui.task_widgets import HabitWidget, DailyWidget, TodoWidget, RewardWidget
 
 
-class TaskListView(urwid.LineBox):
+class FilterListView(urwid.LineBox):
     no_filter = (lambda wid: True, 'all')
 
-    def __init__(self, title, tasks, widget_cls, filters=(no_filter,)):
-        # TODO: filters should be task filters, not some widget filters.
-        # it'll help to generalize TaskListView as a subclass of ListView
+    TITLE_FORMAT = '{title} ({filter})'
+    _compose_title = staticmethod(TITLE_FORMAT.format)
 
-        self.title = title
+    list_title = 'Tasks'
+    widget_cls = None
+    task_filters = (no_filter,)
 
-        self.widget_list_model = FilterListProxyModel(
-            MappingListProxyModel(tasks, widget_cls),
-            filter_=self.no_filter[0],
-            cls=urwid.SimpleListWalker)
+    def __init__(self, tasks):
+        self.filters_ring = itertools.cycle(self.task_filters)
+        self.cur_filter = next(self.filters_ring)
 
-        super().__init__(urwid.ListBox(self.widget_list_model.list), title=title)
+        self.filter_model = FilterListProxyModel(tasks, self.cur_filter[0])
 
-        self.filters_list = filters
-        self.filters_ring = itertools.cycle(filters)
-        self.cur_filter = None
-        self.switch_to_next_filter()
+        self.widget_list_model = MappingListProxyModel(
+            self.filter_model, self.widget_cls, cls=urwid.SimpleListWalker)
+
+        super().__init__(
+            urwid.ListBox(self.widget_list_model.list),
+            title=self._compose_title(title=self.list_title, filter=self.cur_filter[1]))
 
     def switch_to_next_filter(self):
         self.cur_filter = next(self.filters_ring)
-        self.set_title('{} ({})'.format(self.title, self.cur_filter[1]))
-        self.widget_list_model.set_filter(self.cur_filter[0])
+        self.set_title(self._compose_title(title=self.list_title, filter=self.cur_filter[1]))
+        self.filter_model.set_filter(self.cur_filter[0])
 
     def keypress(self, size, key):
-        if key in ACCEL_TOGGLE_LIST_MODE:
+        if key in ACCEL_NEXT_LIST_FILTER:
             self.switch_to_next_filter()
         else:
             return super().keypress(size, key)
 
 
-class HabitListView(TaskListView):
+class HabitListView(FilterListView):
+    list_title = 'Habits'
+    widget_cls = HabitWidget
+
     def __init__(self, user):
-        super().__init__('Habits', user.habits, HabitWidget)
+        super().__init__(user.habits)
 
 
-class DailyListView(TaskListView):
+class DailyListView(FilterListView):
+    list_title = 'Dailies'
+    widget_cls = DailyWidget
+    task_filters = (
+        FilterListView.no_filter,
+        (lambda daily: not daily.completed, 'due'),
+        (lambda daily: daily.completed, 'checked')
+    )
+
     def __init__(self, user):
-        super().__init__(
-            'Dailies',
-            user.dailies, DailyWidget,
-            (self.no_filter,
-             (lambda wid: not wid.get_state(), 'due'),
-             (lambda wid: wid.get_state(), 'checked')
-             )
-        )
+        super().__init__(user.dailies)
 
 
-class TodoListView(TaskListView):
+class TodoListView(FilterListView):
+    list_title = 'To-dos'
+    widget_cls = TodoWidget
+    task_filters = (
+        (lambda todo: not todo.completed, 'due'),
+        (lambda todo: todo.completed, 'done')
+    )
+
     def __init__(self, user):
-        super().__init__(
-            "To-dos",
-            user.todos, TodoWidget,
-            ((lambda wid: not wid.get_state(), 'due'),
-             (lambda wid: wid.get_state(), 'done')
-             )
-        )
+        super().__init__(user.todos)
 
 
-class RewardListView(TaskListView):
+class RewardListView(FilterListView):
+    list_title = 'Rewards'
+    widget_cls = RewardWidget
+
     def __init__(self, user):
-        super().__init__('Rewards', user.rewards, RewardWidget)
+        super().__init__(user.rewards)
